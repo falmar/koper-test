2467aa69066dffdd54a71debe542b92bb2b201d6
David Lavieri <daviddlavier@gmail.com>
2017-05-10 15:41:50 -0400

    Remove render dependency and usage

--- src/dependencies.php
+++ src/dependencies.php
@@ -4,11 +4,11 @@
 $container = $app->getContainer();
 
 // view renderer
-$container['renderer'] = function ($c) {
-    $settings = $c->get('settings')['renderer'];
-
-    return new Slim\Views\PhpRenderer($settings['template_path']);
-};
+// $container['renderer'] = function ($c) {
+//     $settings = $c->get('settings')['renderer'];
+//
+//     return new Slim\Views\PhpRenderer($settings['template_path']);
+// };
 
 // monolog
 $container['logger'] = function ($c) {
