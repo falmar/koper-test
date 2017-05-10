fd351b519cdb7fc6044c2073e16056a1d059397c
David Lavieri <daviddlavier@gmail.com>
2017-05-10 16:50:07 -0400

    Add database handler 'dbh' to DIC

--- src/dependencies.php
+++ src/dependencies.php
@@ -3,14 +3,28 @@
 
 $container = $app->getContainer();
 
-// view renderer
-// $container['renderer'] = function ($c) {
-//     $settings = $c->get('settings')['renderer'];
-//
-//     return new Slim\Views\PhpRenderer($settings['template_path']);
-// };
+// database
+/**
+ * @var \Slim\Container $c
+ * @return PDO
+ */
+$container['dbh'] = function ($c) {
+    $db  = $c->get('settings')['db'];
+    $dbh = new PDO("pgsql:host={$db['host']};dbname={$db['name']}", $db['user'], $db['password']);
+
+    $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $dbh->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+    $dbh->setAttribute(PDO::ATTR_STRINGIFY_FETCHES, false);
+    $dbh->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
+
+    return $dbh;
+};
 
 // monolog
+/**
+ * @var \Slim\Container $c
+ * @return Monolog\Logger
+ */
 $container['logger'] = function ($c) {
     $settings = $c->get('settings')['logger'];
     $logger   = new Monolog\Logger($settings['name']);
