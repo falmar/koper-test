fcdc73c408bc73f4ba7745034ce36518d7b409f2
David Lavieri <daviddlavier@gmail.com>
2017-05-16 19:34:19 -0400

    Update tests for Products::add controller method

--- tests/Controllers/ProductsTest.php
+++ tests/Controllers/ProductsTest.php
@@ -153,7 +153,7 @@ class ProductsTest extends BaseTestCase
 
     public function testAddNotAcceptableWithBadAcceptHeader()
     {
-        $request  = $this->createRequest('GET', '/products');
+        $request  = $this->createRequest('POST', '/products');
         $request  = $request->withHeader('Accept', '');
         $response = $this->runApp($request);
 
@@ -167,7 +167,7 @@ class ProductsTest extends BaseTestCase
 
     public function testAddRequestContentTypeJSON()
     {
-        $request  = $this->createRequest('GET', '/products');
+        $request  = $this->createRequest('POST', '/products');
         $request  = $request->withHeader('Accept', 'application/json');
         $response = $this->runApp($request);
 
@@ -183,14 +183,14 @@ class ProductsTest extends BaseTestCase
     {
         $request  = $this->createRequest('POST', '/products');
         $request  = $request->withHeader('Accept', 'application/json');
-        $request  = $request->withHeader('Content-Type', 'application/json');
+        $request  = $request->withAddedHeader('Content-Type', 'application/json');
         $response = $this->runApp($request);
 
         $bodyString = (string)$response->getBody();
         $body       = json_decode($bodyString, true);
 
         $this->assertEquals(400, $response->getStatusCode());
-        $this->assertContains('Request body cannot be empty.', $body['developerMessage']);
+        $this->assertContains('Request body or fields cannot be empty.', $body['developerMessage']);
         $this->assertContains('Bad Request.', $body['userMessage']);
     }
 
@@ -206,8 +206,27 @@ class ProductsTest extends BaseTestCase
         $body       = json_decode($bodyString, true);
 
         $this->assertEquals(400, $response->getStatusCode());
-        $this->assertContains('Request body values cannot be empty.', $body['developerMessage']);
+        $this->assertContains('Request body or fields cannot be empty.', $body['developerMessage']);
         $this->assertContains('Bad Request.', $body['userMessage']);
+    }
+
+    public function testAddBadRequestBadBodyValues()
+    {
+        $body     = [
+            'name'  => 'bleh',
+            'price' => 'not-floating-point'
+        ];
+        $request  = $this->createRequest('POST', '/products', $body);
+        $request  = $request->withHeader('Accept', 'application/json');
+        $request  = $request->withHeader('Content-Type', 'application/json');
+        $response = $this->runApp($request);
+
+        $bodyString = (string)$response->getBody();
+        $body       = json_decode($bodyString, true);
+
+        $this->assertEquals(500, $response->getStatusCode());
+        $this->assertContains('Internal Server Error.', $body['developerMessage']);
+        $this->assertContains('Unexpected error has occurred, try again later.', $body['userMessage']);
     }
 
     public function testAddError500()
@@ -289,8 +308,8 @@ class ProductsTest extends BaseTestCase
         $this->assertArraySubset($body, $responseBody);
         $this->assertEquals($createdAt, $updatedAt);
         $this->assertTrue(
-            $expectedDate < $createdDate &&
-            $expectedDateForward > $createdDate
+            $expectedDate <= $createdDate &&
+            $expectedDateForward >= $createdDate
         );
     }
 }
