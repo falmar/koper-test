6b4338f5afc2445d01f196b2d9695bf725dae7c9
David Lavieri <daviddlavier@gmail.com>
2017-05-23 19:23:36 -0400

    Add InvoiceController integration tests

--- src/routes.php
+++ src/routes.php
@@ -2,13 +2,18 @@
 // Routes
 
 // Migrate database when running dev mode
-if (getenv('SLIM_ENV') !== 'production') {
+if (getenv('SLIM_ENV') === 'development') {
     $app->get('/migrate', function (\Slim\Http\Request $request, \Slim\Http\Response $response) {
-        $product = new \KoperTest\db\Product($this->get('dbh'));
+        $product = new \KoperTest\Migrations\Product($this->get('dbh'));
+        $invoice = new \KoperTest\Migrations\Product($this->get('dbh'));
 
         $product->down();
         $product->up();
         $product->seed();
+
+        $invoice->down();
+        $invoice->up();
+        $invoice->seed();
 
         return $response->withStatus(200);
     });
@@ -20,3 +25,10 @@ $app->get('/products/{id}', \KoperTest\Controllers\ProductsController::class . '
 $app->post('/products', \KoperTest\Controllers\ProductsController::class . ':add');
 $app->put('/products/{id}', \KoperTest\Controllers\ProductsController::class . ':update');
 $app->delete('/products/{id}', \KoperTest\Controllers\ProductsController::class . ':delete');
+
+// Invoice
+$app->get('/invoices', \KoperTest\Controllers\InvoicesController::class . ':collection');
+$app->get('/invoices/{id}', \KoperTest\Controllers\InvoicesController::class . ':get');
+$app->post('/invoices', \KoperTest\Controllers\InvoicesController::class . ':add');
+$app->put('/invoices/{id}', \KoperTest\Controllers\InvoicesController::class . ':update');
+$app->delete('/invoices/{id}', \KoperTest\Controllers\InvoicesController::class . ':delete');
