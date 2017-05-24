65b15d69608e2c85e9b25ba91f83c6957408f5b1
David Lavieri <daviddlavier@gmail.com>
2017-05-24 16:21:56 -0400

    Implement InvoiceProductsModel

--- src/Models/InvoiceProductsModel.php
+++ src/Models/InvoiceProductsModel.php
@@ -32,7 +32,30 @@ class InvoiceProductsModel
      */
     public function get(int $invoiceId, int $productId): array
     {
-        return [];
+        if ($invoiceId <= 0 || $productId <= 0) {
+            return [];
+        }
+
+        /** @var \PDO $dbh */
+        $dbh = $this->container->get('dbh');
+
+        $stmt = $dbh->prepare(
+            'SELECT invoice_id, product_id, price, quantity FROM invoice_products WHERE invoice_id = ? AND product_id = ?;'
+        );
+
+        $stmt->bindValue(1, $invoiceId, \PDO::PARAM_INT);
+        $stmt->bindValue(2, $productId, \PDO::PARAM_INT);
+        $stmt->execute();
+
+        $result = $stmt->fetch(\PDO::FETCH_ASSOC);
+
+        if (!$result) {
+            return [];
+        }
+
+        $result['price'] = (float)($result['price'] ?? 0);
+
+        return $result;
     }
 
     /**
@@ -44,7 +67,25 @@ class InvoiceProductsModel
      */
     public function count(array $params): int
     {
-        return 0;
+        /** @var \PDO $dbh */
+        $count     = 0;
+        $dbh       = $this->container->get('dbh');
+        $invoiceId = $params['invoice_id'];
+
+        if ($invoiceId <= 0) {
+            return 0;
+        }
+
+        $stmt = $dbh->prepare('SELECT COUNT(*) FROM invoice_products WHERE invoice_id = ?;');
+
+        $stmt->bindColumn(1, $count, \PDO::PARAM_INT);
+
+        $stmt->bindValue(1, $params['invoice_id'], \PDO::PARAM_INT);
+
+        $stmt->execute();
+        $stmt->fetch();
+
+        return $count;
     }
 
     /**
@@ -56,7 +97,55 @@ class InvoiceProductsModel
      */
     public function collection(array $params): array
     {
-        return [];
+        /** @var \PDO $dbh */
+        $dbh      = $this->container->get('dbh');
+        $results  = [];
+        $limitStr = '';
+        $orderStr = '';
+
+        $invoiceId = $params['invoice_id'];
+        $limit     = $params['limit'] ?? 25;
+        $offset    = $params['offset'] ?? 0;
+        $sortField = $params['sortField'] ?? '';
+        $sortOrder = $params['sortOrder'] ?? '';
+
+        if ($invoiceId <= 0) {
+            return [];
+        }
+
+        if ($limit && $offset) {
+            $limitStr = " LIMIT {$limit} OFFSET {$offset}";
+        } elseif ($limit) {
+            $limitStr = " LIMIT {$limit}";
+        }
+
+        if ($sortField && $sortOrder) {
+            $orderStr = " ORDER BY {$sortField} {$sortOrder}";
+        }
+
+        $ssql = "
+          SELECT 
+              invoice_id, product_id, price, quantity 
+          FROM invoice_products
+          WHERE invoice_id = ?
+          {$orderStr}
+          {$limitStr}
+        ;";
+
+        /** @var \PDOStatement $stmt */
+        $stmt = $dbh->prepare($ssql);
+
+        $stmt->bindValue(1, $invoiceId, \PDO::PARAM_INT);
+        $stmt->execute();
+
+        while ($row = $stmt->fetch(\PDO::FETCH_ASSOC)) {
+            // perform any formatter if required
+            $row['price'] = (float)($row['price'] ?? 0);
+
+            $results[] = $row;
+        }
+
+        return $results;
     }
 
     /**
@@ -67,7 +156,24 @@ class InvoiceProductsModel
      */
     public function add(array $data): bool
     {
-        return false;
+        /** @var \PDO $dbh */
+        $dbh  = $this->container->get('dbh');
+        $ssql = '
+            INSERT INTO invoice_products
+            (invoice_id, product_id, price, quantity)
+            VALUES 
+            (?, ?, ?, ?);
+        ';
+        $stmt = $dbh->prepare($ssql);
+
+        $stmt->bindValue(1, $data['invoice_id'], \PDO::PARAM_STR);
+        $stmt->bindValue(2, $data['product_id'], \PDO::PARAM_STR);
+        $stmt->bindValue(3, $data['price'], \PDO::PARAM_STR);
+        $stmt->bindValue(4, $data['quantity'], \PDO::PARAM_INT);
+
+        $stmt->execute();
+
+        return $stmt->rowCount() > 0;
     }
 
     /**
@@ -80,7 +186,28 @@ class InvoiceProductsModel
      */
     public function update(int $invoiceId, int $productId, array $data): bool
     {
-        return false;
+        if ($invoiceId <= 0 || $productId <= 0 || !count($data)) {
+            return false;
+        }
+
+        /** @var \PDO $dbh */
+        $dbh = $this->container->get('dbh');
+
+        $ssql = "
+            UPDATE invoice_products 
+            SET price = ?, quantity = ?
+            WHERE invoice_id = ? AND product_id = ?;                    
+        ";
+
+        $stmt = $dbh->prepare($ssql);
+
+        $stmt->bindValue(1, $data['price'], \PDO::PARAM_STR);
+        $stmt->bindValue(2, $data['quantity'], \PDO::PARAM_INT);
+        $stmt->bindValue(3, $invoiceId, \PDO::PARAM_INT);
+        $stmt->bindValue(4, $productId, \PDO::PARAM_INT);
+        $stmt->execute();
+
+        return $stmt->rowCount() > 0;
     }
 
     /**
@@ -92,6 +219,19 @@ class InvoiceProductsModel
      */
     public function delete(int $invoiceId, int $productId): bool
     {
-        return false;
+        if ($invoiceId <= 0 || $productId <= 0) {
+            return false;
+        }
+
+        /** @var \PDO $dbh */
+        $dbh  = $this->container->get('dbh');
+        $ssql = 'DELETE FROM invoice_products WHERE invoice_id = ? AND product_id = ?;';
+        $stmt = $dbh->prepare($ssql);
+
+        $stmt->bindValue(1, $invoiceId, \PDO::PARAM_INT);
+        $stmt->bindValue(2, $productId, \PDO::PARAM_INT);
+        $stmt->execute();
+
+        return $stmt->rowCount() > 0;
     }
 }
